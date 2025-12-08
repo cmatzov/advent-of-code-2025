dfe9be779203fe6c3481a83314317a8b93125c24
cmatzov <cristian.matzov@gmail.com>
2025-12-08 17:22:42 +0200

    day8

---
+++ day8/part_one.py
@@ -0,0 +1,50 @@
+import math
+
+f8 = open("day8/input8.txt")
+boxes_locations = [[int(location) for location in box.split(",")] for box in f8.read().splitlines()]
+
+parent = list(range(len(boxes_locations)))
+
+def calculate_distance(a, b):
+    distance = math.sqrt(pow((a[0] - b[0]), 2) + pow((a[1] - b[1]), 2) + pow((a[2] - b[2]), 2))
+    return distance
+
+def shortest_distance(arr):
+    return sorted(arr, key=lambda x: x[0])
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def main():
+    distances = []
+    for i in range(len(boxes_locations)):
+        for j in range(i + 1, len(boxes_locations)):
+            distance = tuple((calculate_distance(boxes_locations[i], boxes_locations[j]), i, j))
+            distances.append(distance)
+    boxes = shortest_distance(distances)
+    circuits = []
+    for box in boxes:
+        circuits.append([box[1], box[2]])
+
+    # From here I started cheating
+
+    size = [1] * len(boxes_locations)
+
+    for a, b in circuits[:1000]:
+        rootA = find(a)
+        rootB = find(b)
+        if rootA != rootB:
+            parent[rootB] = rootA
+            size[rootA] += size[rootB]
+
+    component_sizes = [size[i] for i in range(len(boxes_locations)) if parent[i] == i]
+    component_sizes.sort(reverse=True)
+
+    result = component_sizes[0] * component_sizes[1] * component_sizes[2]
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
